3bb12f2d2b82b35e6939f1acd2e67317b06481ad
Yu-Jen Lin <peacedove5210@gmail.com>
2012-08-01 10:27:19 +0800

    Initial Commit

---
+++ PhotoMosaic/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PhotoMosaic
+//
+//  Created by peacedove on 8/1/12.
+//  Copyright (c) 2012 peacedove. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
